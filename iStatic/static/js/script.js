1c5b03a82f58ef7f4988e87843b9123b92d8d8ac
mac <nicholasdwoodward@gmail.com>
2017-01-17 19:29:24 -0500

    fade in and new assets

--- iStatic/static/js/script.js
+++ iStatic/static/js/script.js
@@ -1,9 +1,22 @@
 var supportsAutoplay = false;
 var waitTime;
 
+var topWait = 5000;
+var bottomWait = 10000;
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+var randNum = getRandomInt(1,7);
+
+var assetName = 'Asset_' + randNum.toString() + '_CATALA_PAF_LinkNYC_02.06.17';
+
 $('#bg-video').vide({
-	mp4: 'static/media/video/video_1.mp4',
-	poster: 'static/media/img/still_1.jpg',
+	mp4: 'static/media/video/' + assetName + '.mp4',
+	poster: 'static/media/img/' + assetName + '.mp4',
 }, {
 	posterType: 'jpg',
 	origin: 'content-box',
@@ -17,12 +30,25 @@ setTimeout(          //wait for listener to run
     function(){
         if(supportsAutoplay){
         	video.onloadeddata = function(){
-	$('#bg-video-box').addClass('shown');
-}
+            	$('#bg-video-box').addClass('shown');
+            }
         }
         else {
         	$('#bg-video-box').addClass('shown');
         }
     },
     waitTime
-); 
+);
+
+setTimeout(          //wait for listener to run
+    function(){
+        $('.top').removeClass('hidden');
+    },
+    topWait
+)
+setTimeout(          //wait for listener to run
+    function(){
+        $('.bottom').removeClass('hidden');
+    },
+    bottomWait
+)
