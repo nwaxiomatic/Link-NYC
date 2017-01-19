0ce90231624e7af4ed96abcc94ecbf1e13f5c929
mac <nicholasdwoodward@gmail.com>
2017-01-19 11:03:11 -0500

    final cleanup

--- iStatic/static/js/script.js
+++ iStatic/static/js/script.js
@@ -1,8 +1,8 @@
 var supportsAutoplay = false;
 var waitTime;
 
-var topWait = 3000;
-var bottomWait = 6000;
+var topWait = 0;
+var bottomWait = 0;
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -15,40 +15,21 @@ var randNum = getRandomInt(1,7);
 var assetName = 'Asset_' + randNum.toString() + '_CATALA_PAF_LinkNYC_02.06.17';
 
 $('#bg-video').vide({
-	//mp4: 'static/media/video/' + assetName + '.mp4',
 	poster: 'static/media/img/' + assetName + '.mp4',
 }, {
 	posterType: 'jpg',
 	origin: 'content-box',
 });
-instance = $('#bg-video').data('vide');
-video = instance.getVideoObject();
-video.addEventListener('play', function () {
-    supportsAutoplay = true;
-});
 setTimeout(          //wait for listener to run
     function(){
-        if(supportsAutoplay){
-        	video.onloadeddata = function(){
-            	$('#bg-video-box').addClass('shown');
-            }
-        }
-        else {
-        	$('#bg-video-box').addClass('shown');
-        }
+        $('#bg-video-box').addClass('shown');
     },
     waitTime
 );
 
 setTimeout(          //wait for listener to run
     function(){
-        $('.top').removeClass('hidden');
+        $('.center').addClass('shown');
     },
     topWait
-)
-setTimeout(          //wait for listener to run
-    function(){
-        $('.bottom').removeClass('hidden');
-    },
-    bottomWait
 )
