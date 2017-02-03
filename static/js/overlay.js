d1c6499474b42febcca359ce4938a18337cb3338
mac <nicholasdwoodward@gmail.com>
2017-02-02 23:50:19 -0500

    updatesss

--- static/js/overlay.js
+++ static/js/overlay.js
@@ -1,3 +1,10 @@
+$('#overlay-container').addClass('shown');
 $('#overlay').click(function(){
 	$('#overlay').toggleClass('slideUp');
+	if($('#overlay').hasClass('slideUp')){
+		$('.overlay-section').removeClass('shown');
+	}
+});
+$('.expand-overlay').click(function(){
+	$('#' + this.id.replace('expand-', '')).addClass('shown');
 });
